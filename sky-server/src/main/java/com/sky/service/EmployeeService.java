44ef433ecb5692b853a8c6077fc42a27b7a648be
Bouboo1111 <3548345391@qq.com>
2026-04-09 17:52:14 +0800

    员工分页查询业务开发

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -1,8 +1,13 @@
 package com.sky.service;
 
+import com.github.pagehelper.Page;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.result.PageResult;
+
+import java.util.List;
 
 public interface EmployeeService {
 
@@ -13,4 +18,5 @@ public interface EmployeeService {
      */
     Employee login(EmployeeLoginDTO employeeLoginDTO);
     void save(EmployeeDTO employeeDTO);
+    PageResult page(EmployeePageQueryDTO employeePageQueryDTO);
 }
