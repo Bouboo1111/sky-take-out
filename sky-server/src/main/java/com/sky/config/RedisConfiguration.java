71aff51825f12ed6f98d0345d54d1c1fa3e055f6
Bouboo1111 <3548345391@qq.com>
2026-04-16 16:43:45 +0800

    缓存套餐代码开发

--- sky-server/src/main/java/com/sky/config/RedisConfiguration.java
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -1,14 +1,22 @@
 
 package com.sky.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import com.sky.json.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @Configuration
 @Slf4j
@@ -32,5 +40,29 @@ public class RedisConfiguration {
 
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory){
+        log.info("开始创建RedisCacheManager对象...");
+
+        ObjectMapper objectMapper = new JacksonObjectMapper();
+        objectMapper.activateDefaultTyping(
+                LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.NON_FINAL,
+                JsonTypeInfo.As.WRAPPER_OBJECT
+        );
+
+        GenericJackson2JsonRedisSerializer jsonSerializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofHours(2))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jsonSerializer))
+                .disableCachingNullValues();
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(config)
+                .build();
     }
 }
--- sky-server/src/main/java/com/sky/controller/admin/SetMealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetMealController.java
@@ -1,13 +1,17 @@
 package com.sky.controller.admin;
 
+import com.sky.constant.StatusConstant;
 import com.sky.dto.SetmealDTO;
 import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.entity.Setmeal;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.SetmealService;
 import com.sky.vo.SetmealVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,6 +28,7 @@ public class SetMealController {
      * @return
      */
     @PostMapping
+    @CacheEvict(cacheNames = "setmeal",key = "#setmealDTO.categoryId")
     public Result save(@RequestBody SetmealDTO setmealDTO){
         log.info("新增套餐：{}",setmealDTO);
         setMealService.saveWithDish(setmealDTO);
@@ -47,6 +52,7 @@ public class SetMealController {
      * @param ids
      */
     @DeleteMapping
+    @CacheEvict(cacheNames = "setmeal",allEntries = true)
     public Result delete(@RequestParam List<Long> ids){
         log.info("批量删除：{}",ids);
         setMealService.deleteBatch(ids);
@@ -58,6 +64,7 @@ public class SetMealController {
      * @param id
      */
     @PostMapping("/status/{status}")
+    @CacheEvict(cacheNames = "setmeal",allEntries = true)
     public Result startOrStop(@PathVariable Integer status, Long id){
         setMealService.startOrStop(status,id);
         return Result.success();
@@ -81,9 +88,26 @@ public class SetMealController {
      * @return
      */
     @PutMapping
+    @CacheEvict(cacheNames = "setmeal",allEntries = true)
     public Result update(@RequestBody SetmealDTO setmealDTO){
         log.info("修改套餐：{}",setmealDTO);
         setMealService.updateWithDish(setmealDTO);
         return Result.success();
+    }
+
+    /**
+     * 根据分类id查询套餐
+     * @param categoryId
+     * @return
+     */
+    @GetMapping("/list")
+
+    public Result<List<Setmeal>> list(Long categoryId){
+        log.info("根据分类id查询套餐：{}",categoryId);
+        Setmeal setmeal = new Setmeal();
+        setmeal.setCategoryId(categoryId);
+        setmeal.setStatus(StatusConstant.ENABLE);
+        List<Setmeal> list = setMealService.list(setmeal);
+        return Result.success(list);
     }
 }
